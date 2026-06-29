242ab75e18ccd40c436941c792c63317460ab999
Anastasiia <anastasiia.hainova@gmail.com>
2026-06-29 20:36:32 +0200

    Main URLS and VIEWS in SHOP APP created

--- shop/urls.py
+++ shop/urls.py
@@ -1,2 +1,14 @@
 from django.urls import path
+from shop.views import index, about, contact, product_list, product_detail, login_view, register_view, logout_view
 
+
+urlpatterns = [
+    path("", index, name="main_page"),
+    path("about/", about, name="about_page"),
+    path("contact/", contact, name="contact_page"),
+    path("products/", product_list, name="products"),
+    path("products/<int:pk>/", product_detail, name="product_detail"),
+    path("login/", login_view, name="login_view"),
+    path("register/", register_view, name="register_view"),
+    path("logout/", logout_view, name="logout_view")
+]
