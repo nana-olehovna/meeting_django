347619e7bb535233e46280cdc59e8635b0fe5aef
Anastasiia <anastasiia.hainova@gmail.com>
2026-07-08 12:15:26 +0200

    Models created. Migrations made.

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    balance = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=10000.00,
+    )
+    create_at = models.DateTimeField(auto_now_add=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, null=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    create_at = models.DateTimeField(auto_now_add=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, null=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='products', blank=True, null=True)
+    create_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    create_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    products = models.ManyToManyField(Product, through='CartItem')
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    item = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_item')
+    quantity = models.IntegerField()
+    create_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    create_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    products = models.ManyToManyField(Product, through='OrderItem')
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+    quantity = models.IntegerField()
+    fixed_price = models.DecimalField(max_digits=8, decimal_places=2)
+    create_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+class Purchase(models.Model):
+    item = models.OneToOneField(OrderItem, on_delete=models.CASCADE, related_name='purchase')
+    paid_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    create_at = models.DateTimeField(auto_now_add=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, null=True)
